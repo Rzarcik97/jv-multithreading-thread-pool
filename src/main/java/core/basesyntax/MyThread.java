e9c6e37da653e35181ad536d1202f92441e864f4
Karol Gajda <karolgaj97@interia.pl>
2026-01-20 12:36:39 +0100

    adding future threat

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,20 @@
 package core.basesyntax;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final Logger log = LogManager.getLogger(MyThread.class);
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int value = (new Random().nextInt(5) + 1) * 100;
+        Thread.sleep(value);
+        log.info("Task duration was " + value + " ms, execution finished at " + LocalDateTime.now());
+        return "Task duration was " + value + " ms, execution finished at " + LocalDateTime.now();
     }
 }
